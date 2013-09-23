dfc2358412c42199cce7cca3f0a2bf541f632ba1
isxxguo <isxxguo@pptv.com>
2013-09-23 07:56:23 +0000

    /ppbox/branches/1.2.0 /src/ppbox/ 调整 ClassFactory, UrlSource UrlSink，移动到 util 库 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@7448 b3a1389e-2159-5c43-9038-463bf877d0d9

--- ClassRegister.h
+++ ClassRegister.h
@@ -3,7 +3,7 @@
 #ifndef _PPBOX_MMSPD_CLASS_REGISTER_H_
 #define _PPBOX_MMSPD_CLASS_REGISTER_H_
 
-#include <ppbox/common/ClassRegister.h>
+#include <util/tools/ClassRegister.h>
 
 #include "ppbox/mmspd/MmsMuxer.h"
 
